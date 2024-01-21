ad7a893a4869fab9b13af0609caf19ef22264204
Hyeonghwan <khhh9401@gmail.com>
2024-01-22 00:11:13 +0900

    test

--- project/api/app.py
+++ project/api/app.py
@@ -12,7 +12,7 @@ async def root():
 @app.get("/scrape")
 async def scape(url: str):
     encoded_url = encode_url(url)
-    path = f"./html/{encoded_url}.txt"
+    path = f"./dynamic-vol/html/{encoded_url}.txt"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
     with open(path, "w", encoding="utf-8") as f:
         html = requests.get(url, headers=headers).text
