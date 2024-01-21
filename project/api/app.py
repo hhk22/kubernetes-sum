f9c8804ac2c8cba19d55886b3d3f914b960a8c9a
Hyeonghwan <khhh9401@gmail.com>
2024-01-22 00:21:49 +0900

    test

--- project/api/app.py
+++ project/api/app.py
@@ -16,5 +16,6 @@ async def scape(url: str):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
     with open(path, "w", encoding="utf-8") as f:
         html = requests.get(url, headers=headers).text
+        print("path", path)
         f.write(html)
         return path
