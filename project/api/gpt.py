2303809a5fca3b2c1de76955a6386a3ea9f8743f
Hyeonghwan <khhh9401@gmail.com>
2024-01-24 17:29:20 +0900

    test

--- project/api/gpt.py
+++ project/api/gpt.py
@@ -1,42 +1,36 @@
 
 import glob
-import os
-import json
-import requests
-from dotenv import load_dotenv
-load_dotenv()
+import time
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain.callbacks import get_openai_callback
+
+
+def query_openai(prompt, api_key):
+    chat = ChatOpenAI(api_key=api_key)
+
+    messages = [
+        SystemMessage(content="You're a helpful assistant"),
+        HumanMessage(content=prompt)
+    ]
+
+    result = None
+    with get_openai_callback() as cb:
+        result = chat.invoke(messages).content
+        print(cb)
+    return result
 
 def run():
 
-    # files = glob.glob("./sentences/*")
     files = glob.glob("/api/dynamic-vol/sentences/*")
     file_path = files[0]
-
-    print(file_path)
 
     text = None
     with open(file_path, encoding="utf-8") as f:
         text = f.read()
     # os.remove(file_path)
-    print(text)
-
-    headers = {
-        "Authorization": f"Bearer {os.environ.get('OPENAI_API_KEY')}",
-        "Content-Type": 'application/json'
-    }
-
-    data = json.dumps({
-        "model": "gpt-3.5-turbo",
-        "messages": [
-            {"role": "system", "content": "You are a shopping mall administrator. You have sell products to customer. Use the information below to make useful sentences"},
-            {"role": "user", "content": text}
-        ],
-        "temperature": 0.7
-    })
-    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, data=data)
-    json_response = json.loads(response.text)
-    print(json_response['choices'][0]['message']['content'])
-
+    rst = query_openai(text)
+    print(rst)
     
     # basename = os.path.basename(file_path)
     # with open(f"/api/dynamic-vol/sentences/{basename}", "w", encoding="utf-8") as f:
@@ -45,13 +39,12 @@ def run():
     # print(sentences)
 
 if __name__ == "__main__":
-    run()
-    # while True:
-    #     try:
-    #         print('scraping...')
-    #         run()
-    #         time.sleep(10)
-    #         break
-    #     except Exception as e:
-    #         time.sleep(5)
-    #         print(e)
+    while True:
+        try:
+            print('gpting...')
+            run()
+            time.sleep(10)
+            break
+        except Exception as e:
+            time.sleep(5)
+            print(e)
